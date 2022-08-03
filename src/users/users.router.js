ee5414390296764f5c856f4438dbacfa3f6e0711
Joseph Spencer <104227087+Jarseph@users.noreply.github.com>
2022-08-03 15:14:09 -0700

    Add files via upload

---
+++ src/users/users.router.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const controller = require("./users.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+
+const corsGet = cors({methods: "GET"});
+
+router
+  .route("/:userId")
+  .get(corsGet, controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
+  .options(corsGet)
+  .all(methodNotAllowed);
+
+router
+  .route("/")
+  .get(corsGet, controller.list) 
+  .post(controller.create)
+  .options(corsGet)
+  .all(methodNotAllowed);
+
+module.exports = router;
